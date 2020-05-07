0dd550177216c8f52ca29c3bb7e740639373f0cc
inteuniso <dzferry9@gmail.com>
2020-05-07 14:56:31 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cos_urban_mobility_tracker',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Dakota Ferry',
+    author_email='',
+    description=''
+)
