26948de26d625e5a5398bb6b4c2b8b92ba891e40
inteuniso <dzferry9@gmail.com>
2020-05-20 12:38:15 -0500

    Version 5/20/20

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const latestFile = require('../utils/latestFileGrab');
 
 const async = require('async');
-const { createCanvas, loadImage } = require('canvas');
 
 
 exports.index = function(req, res) {
-    let lastframe = latestFile.getLatestFile("output");
-    res.render('index', { title:'Traffic Counting Camera', data: lastframe });
-}
+
+  async.parallel({
+    latest_frame: function(){
+      latestFile.getLatestFile("output");
+    }
+    image: function(){
+
+    }
+
+  }, function(err, results){
+    res.render('index', { title:'Traffic Counting Camera', error: err, data: results });
+});
+};
